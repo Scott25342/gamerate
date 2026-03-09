bed62330a989bdaa9eadbb4755f09fc986ad5871
Scott Burgess <2467497b@student.gla.ac.uk>
2026-03-09 13:08:45 +0000

    VideoGame model

--- gamerate_app/models.py
+++ gamerate_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class VideoGame(models.Model):
+    title = models.CharField(max_length=200)
+    genre = models.CharField(max_length=200)
+    rating = models.FloatField(blank=True, null=True)
+    release_year = models.IntegerField()
+    developer = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
