b0bdbcb5fc1357b7037e1c42f76624cf54f4d7f0
Scott Burgess <2467497b@student.gla.ac.uk>
2026-03-11 12:06:31 +0000

    game library/page

--- gamerate_app/urls.py
+++ gamerate_app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('game_library/', views.game_library, name='game_library'),
+    path('game_detail/<int:game_id>/', views.game_detail, name='game_detail'),
 ]
--- gamerate_app/views.py
+++ gamerate_app/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import VideoGame
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def game_detail(request, game_id):
+    game = get_object_or_404(VideoGame, pk=game_id)
+    return render(request, 'game_detail.html', {'game': game})
+
+def game_library(request):
+    games = VideoGame.objects.all()
+    return render(request, 'game_library.html', {'games': games})
