1b8c81de9dc7e89c9959942efaa4804fdf519cc5
Scott Burgess <2467497b@student.gla.ac.uk>
2026-03-13 12:08:29 +0000

    add/edit/delete games

--- gamerate_app/urls.py
+++ gamerate_app/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('game_library/', views.game_library, name='game_library'),
     path('game_detail/<int:game_id>/', views.game_detail, name='game_detail'),
+    path("edit_game/<int:game_id>/", views.edit_game, name="edit_game"),
+    path("delete_game/<int:game_id>/", views.delete_game, name="delete_game"),
+    path("add_game/", views.add_game, name="add_game"),
 
     path("profile/", views.profile_page, name="profile"),
     path("delete_review/<int:review_id>/", views.delete_review, name="delete_review"),
--- gamerate_app/views.py
+++ gamerate_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.contrib.admin.views.decorators import staff_member_required
 from .models import VideoGame, Review
 
 
@@ -56,3 +57,55 @@ def edit_review(request, review_id):
         return redirect("profile")
 
     return render(request, "edit_review.html", {"review": review})
+
+@staff_member_required
+def edit_game(request, game_id):
+    game = get_object_or_404(VideoGame, id=game_id)
+
+    if request.method == "POST":
+        game.title = request.POST.get("title")
+        game.genre = request.POST.get("genre")
+        game.rating = request.POST.get("rating")
+        game.release_year = request.POST.get("release_year")
+        game.developer = request.POST.get("developer")
+        game.description = request.POST.get("description")
+        game.image = request.POST.get("image")
+        game.save()
+        return redirect("game_detail", game_id=game.id)
+
+    return render(request, "edit_game.html", {"game": game})
+
+
+@staff_member_required
+def delete_game(request, game_id):
+    game = get_object_or_404(VideoGame, id=game_id)
+
+    if request.method == "POST":
+        game.delete()
+        return redirect("game_library")
+
+    return render(request, "delete_game.html", {"game": game})
+
+@staff_member_required
+def add_game(request):
+    if request.method == "POST":
+        title = request.POST.get("title")
+        genre = request.POST.get("genre")
+        rating = request.POST.get("rating") or None
+        release_year = request.POST.get("release_year")
+        developer = request.POST.get("developer")
+        description = request.POST.get("description")
+        image = request.POST.get("image")
+
+        game = VideoGame.objects.create(
+            title=title,
+            genre=genre,
+            rating=rating,
+            release_year=release_year,
+            developer=developer,
+            description=description,
+            image=image
+        )
+        return redirect("game_detail", game_id=game.id)
+
+    return render(request, "add_game.html")
